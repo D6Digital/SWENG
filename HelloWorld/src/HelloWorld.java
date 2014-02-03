0df40228fa84ff4957c0eaab86476ce542bc4cc0
D6Digital <d6digital2014@gmail.com>
2014-02-03 22:09:59 +0000

    Hello World Project

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hey Baby!!!");
+		System.out.println("Heya !!!");
+		for (int e = 0; e < args.length; e++) {
+			
+		}
+		
+	}
+
+}
