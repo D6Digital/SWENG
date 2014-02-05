0f06881bb2cb70bddc129c844f7f78cf6056f82c
samPick <sp807@.york.ac.uk>
2014-02-05 14:59:20 +0000

    Changed hello world to have a little window

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,47 @@
+import java.awt.Color;
+import java.awt.Font;
 
-public class HelloWorld {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class HelloWorld extends JLabel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	 public HelloWorld(String text) {
+		super(text);
+	}
+
+	private static void createAndShowGUI() {
+	        //Create and set up the window.
+	        JFrame frame = new JFrame("Hello Label");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	        
+	        String text = "Hello World!";
+	        //Add contents to the window.
+	        frame.add(new HelloWorld(text));
+
+	        //Display the window.
+	        frame.pack();
+	        frame.setVisible(true);
+	    }
 
 	public static void main(String[] args) {
+		
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+		
 		
 		System.out.println("Person:   Hey SWENG Group 5!!!");
 		System.out.println("Us: 	  Hi :)");
