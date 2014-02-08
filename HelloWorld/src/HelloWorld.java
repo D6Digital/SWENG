f5a3e5641ea756848431be072aebb7bbdf8217b1
D6Digital <d6digital2014@gmail.com>
2014-02-08 20:18:59 +0000

    Example of specific coordinate placement added

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,6 @@
+import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -13,18 +16,30 @@ public class HelloWorld extends JLabel{
 	
 	 public HelloWorld(String text) {
 		super(text);
+		this.setBounds(0, 0, 40, 20);
 	}
 
 	private static void createAndShowGUI() {
 	        //Create and set up the window.
 	        JFrame frame = new JFrame("Hello Label");
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+	        frame.setLayout(null);
+	        
+	        // Not using a layout manager so a size for the frame
+	        // should be decided otherwise the minimum is nothing
+	        Dimension minimumSize = new Dimension(600, 400);
+	        frame.setMinimumSize(minimumSize);
 	        
 	        String text = "Hello World!";
+	        
+	        // JButton placed at specific co-ordinates
+	        JButton button1 = new JButton("OK");
+	        button1.setBounds(40, 200, 75, 75);
+	        
 	        //Add contents to the window.
 	        frame.add(new HelloWorld(text));
-
+	        frame.add(button1);
+	        
 	        //Display the window.
 	        frame.pack();
 	        frame.setVisible(true);
