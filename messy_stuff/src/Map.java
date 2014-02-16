ccc52d1cac959bfbe5c9cc0adfbfc9e44e9c608e
samPick <sp807@york.ac.uk>
2014-02-16 14:56:31 +0000

    Some experiments including video using vlcj, which needs alot of cleaning up

---
+++ messy_stuff/src/Map.java
@@ -0,0 +1,13 @@
+
+public class Map {
+
+	Shape MapShape = new Shape(0,100,100);
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ messy_stuff/src/Shape.java
@@ -0,0 +1,15 @@
+
+public class Shape {
+
+	int type = 0;
+	int width = 0;
+	int height = 0;
+	public Shape(int type, int width, int height) {
+		super();
+		this.type = type;
+		this.width = width;
+		this.height = height;
+	}
+	
+	
+}
